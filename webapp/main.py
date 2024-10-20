71dc7cd50b7a2c121e6158da3f270752fa3b6b83
Braulio Fernandes <brauliopf+github@gmail.com>
2024-10-20 19:54:02 -0400

    creates plotly charts and adds email generation

--- webapp/main.py
+++ webapp/main.py
@@ -59,6 +59,7 @@ def prepare_input(credit_score, location, gender, age, tenure, balance,
     input_df = pd.DataFrame([input_dict])
     return input_df, input_dict
 
+import utils as ut
 def make_predictions(input_df):
     '''Prints 3 predictions and the average of 3 models: XGBoost, Random Forest, KNN.
     Returns the average of the predictions of the 3 models'''
@@ -68,25 +69,38 @@ def make_predictions(input_df):
         'K-Nearest Neighbors': knn_model.predict_proba(input_df)[0][1],
     }
     avg_probability = np.mean(list(probabilities.values()))
-    
-    st.markdown("### Model Probabilities")
-    for model, prob in probabilities.items():
-        st.write(f"{model}: {prob}")
-    st.write(f"Average Probability: {avg_probability}")
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+       fig = ut.create_gauge_chart(avg_probability)
+       st.plotly_chart(fig, use_contaioner_width=True)
+       st.write(f"The customer has {avg_probability:.2%} probability of churning")
+
+    with col2:
+       fig_probs = ut.create_model_probability_chart(probabilities)
+       st.plotly_chart(fig_probs, use_container_width=True)
 
     return avg_probability 
 
-def explain_prediction(probability, input_dict, surname, df):
+def explain_prediction(probability, input_dict, surname, df, model="llama-3.2-3b-preview"):
     '''Engage with an LLM to explain the prediction of the machine learning model based on
     feature importances and customer data, and summary statistics of churned and non-churned customers'''
 
     prompt = f"""You are an expert data scientist at a bank, where you specialize in interpreting and explaining predictions of machine learning models.
-    Your machine learning model calculated the probability that a customer will churn. The information is shared below.
+    You are given the probability of churn and must explain the model's prediction based on your assessment of the context: customer's information, the summary statistics of churned and non-churned customers, and the feature importances provided.
 
+    - Do not mention the probability of churning, or the machine learning model, or say anything like "Based on the machine learning model's prediction and top 10 most important features", just explain the prediction.
+    - Use the model's prediction. Do not make up your own assessment. Use this criteria:
+      - [Riosk of churning > 30%] Custoner is at risk ofhcuirning. Generate a 3 sentence explanation of why they are at risk of churning.
+      - [Risk of churning < 30%] Customer might not be at risk of churning. Generate a 2 sentence explanation of why they might not be at risk of churning.
+
+    Customer information is shared below.
+    
     Customer information:
     Name: {surname}
-    Risk of churning: {round(probability * 100, 1)}%
-    {input_dict}
+    Risk of churning: {round(probability * 100, 1)}% (decimal: {probability})
+    General customer data: {input_dict}
 
     Here are the machine learning model's top 10 most important features for predicting churn:
           Feature             | Importance
@@ -104,26 +118,12 @@ def explain_prediction(probability, input_dict, surname, df):
           HasCrCard           | 0.031940
           Tenure              | 0.030054
           Gender_Male         | 0.000000
-
-    {pd.set_option('display.max_columns', None)}
-
-    Here are summary statistics for churned customers:
-    {df[df['Exited'] == 1].describe()}
-
-    Here are summary statistics for non-churned customers:
-    {df[df['Exited'] == 0].describe()}
-
-    - If the customer has over a 60% risk of churning, generate a 3 sentence explanation of why they are at risk of churning.
-    - If the customer has less than a 60% risk of churning, generate a 2 sentence explanation of why they might not be at risk of churning.
-    - Your explanation should be based on the customer's information, the summary statistics of churned and non-churned customers, and the feature importances provided.
-
-    Don't mention the probability of churning, or the machine learning model, or say anything like "Based on the machine learning model's prediction and top 10 most important features", just explain the prediction.
 """
 
     print("EXPLANATION PROMPT", prompt)
 
     raw_response = client.chat.completions.create(
-        model="llama-3.2-3b-preview",
+        model=model,
         messages=[{
             "role": "user",
             "content": prompt
@@ -133,9 +133,12 @@ def explain_prediction(probability, input_dict, surname, df):
     return raw_response.choices[0].message.content
    
 def build_UI(selected_customer):
-      '''Use 2 columns. Declaring an st (Streamlit) object adds it to the session.
-      Returns the output of prepare_input(...): (input_df, input_dict)
       '''
+      Use 2 columns.
+      Declaring an st (Streamlit) object adds it to the UI and session.
+      Returns: output of prepare_input(...): (input_df, input_dict)
+      '''
+      
       col1, col2 = st.columns(2)
 
       with col1:
@@ -189,10 +192,60 @@ def build_UI(selected_customer):
           "Estimated Salary",
           min_value=0.0,
           value=float(selected_customer['EstimatedSalary']))
-        
+
       return prepare_input(credit_score, location, gender, age,
                                           tenure, balance, num_products, has_credit_card,
                                           is_active_member, estimated_salary)
+
+def compose_email(probability, input_dict, explanation, surname):
+  """
+  Generates a personalized email to a customer based on their churn probability.
+
+  Args:
+      probability (float): The probability of the customer churning.
+      input_dict (dict): A dictionary containing the customer's information.
+      explanation (str): An explanation of why the customer might be at risk of churning.
+      surname (str): The customer's surname.
+
+  Returns:
+      str: The generated email.
+  """
+
+  prompt = f"""You are a manager at HS Bank. You are responsible for
+  ensuring customers stay with the bank and are incentivized with
+  various offers.
+
+  You noticed a customer named {surname} has a {round(probability * 100, 1)}% probability of churning.
+
+  Here is the customer's information:
+  {input_dict}
+
+  Here is some explanation as to why the customer might be at risk
+  of churning:
+  {explanation}
+
+  Generate an email to the customer based on their information,
+  asking them to stay if they are at risk of churning, or offering them
+  incentives so that they become more loyal to the bank.
+
+  Make sure to list out a set of incentives to stay based on their
+  information, in bullet point format. Don't ever mention the
+  probability of churning, or the machine learning model to the
+  customer.
+  """
+
+  raw_response = client.chat.completions.create(
+    model="llama-3.1-8b-instant",
+    messages=[{
+       "role": "user",
+       "content": prompt
+    }],
+
+  )
+
+  print("\n\nEMAIL PROMPT", prompt)
+
+  return raw_response.choices[0].message.content   
 
 def main():
   st.title("Customer Churn Prediction")
@@ -206,15 +259,20 @@ def main():
   if selected_customer_option:
     selected_customer_id = int(selected_customer_option.split(" - ")[0])
     selected_customer = df.loc[df['CustomerId'] == selected_customer_id].iloc[0]
-    
     input_df, input_dict = build_UI(selected_customer)
 
     load_models()
     avg_probability = make_predictions(input_df)
-    explanation = explain_prediction(avg_probability, input_dict, selected_customer['Surname'], df)
+    print('AVG PROB:', avg_probability)
+    explanation = explain_prediction(avg_probability, input_dict, selected_customer['Surname'], df, "llama-3.1-70b-versatile")
     st.markdown("---")
     st.subheader("Explanation of Prediction")
     st.markdown(explanation)
+
+    email = compose_email(avg_probability, input_dict, explanation, selected_customer['Surname'])
+    st.markdown("---")
+    st.subheader("Personalized Email")
+    st.markdown(email)
 
   else:
     st.write("Please select a customer to proceed.")
